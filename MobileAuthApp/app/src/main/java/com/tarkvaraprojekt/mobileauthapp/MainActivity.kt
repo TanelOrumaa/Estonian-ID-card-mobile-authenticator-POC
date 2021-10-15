48817f90572abac9fea91cab719a46c51121ad22
Henrik Lepson <henrikk895@gmail.com>
2021-10-15 17:06:00 +0300

    MOB-38 created basic menu layout, cur no actions

--- MobileAuthApp/app/src/main/java/com/tarkvaraprojekt/mobileauthapp/MainActivity.kt
+++ MobileAuthApp/app/src/main/java/com/tarkvaraprojekt/mobileauthapp/MainActivity.kt
@@ -4,6 +4,9 @@ import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.tarkvaraprojekt.mobileauthapp.databinding.ActivityMainBinding
@@ -17,8 +20,29 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navigationController = navHostFragment.navController
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.menu_language_option -> {
+            Toast.makeText(this, getString(R.string.menu_action_unavailable), Toast.LENGTH_SHORT)
+                .show()
+            true
+        }
+        R.id.menu_settings_option -> {
+            Toast.makeText(this, getString(R.string.menu_action_unavailable), Toast.LENGTH_SHORT)
+                .show()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 
 }
