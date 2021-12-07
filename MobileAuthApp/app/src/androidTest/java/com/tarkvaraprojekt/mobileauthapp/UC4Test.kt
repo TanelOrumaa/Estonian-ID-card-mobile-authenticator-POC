fc3161be512d818d0e4e99fb92f703de41007565
stargateprovider <kana46@windowslive.com>
2021-12-07 11:02:11 +0200

    Moved UC4test to new branch

---
+++ MobileAuthApp/app/src/androidTest/java/com/tarkvaraprojekt/mobileauthapp/UC4Test.kt
@@ -0,0 +1,62 @@
+package com.tarkvaraprojekt.mobileauthapp
+
+//import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingPolicies
+import androidx.test.espresso.NoMatchingViewException
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+
+import org.junit.*
+import org.junit.runner.RunWith
+import java.util.concurrent.TimeUnit
+
+@RunWith(AndroidJUnit4::class)
+class UC4Test {
+    @get:Rule
+    var activityActivityTestRule: ActivityTestRule<MainActivity> = ActivityTestRule(
+        MainActivity::class.java
+    )
+
+    @Before
+    fun setUp() {
+        IdlingPolicies.setMasterPolicyTimeout(3, TimeUnit.SECONDS)
+        IdlingPolicies.setIdlingResourceTimeout(3, TimeUnit.SECONDS)
+        activityActivityTestRule.activity
+            .supportFragmentManager.beginTransaction()
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    fun navigateToCANView() {
+        onView(withId(R.id.menu_settings_option)).perform(click())
+        try {
+            // Delete existing CAN
+            onView(withText(R.string.can_delete)).perform(click())
+        } catch (ignore: NoMatchingViewException) {}
+
+        onView(withId(R.id.can_menu_action)).perform(click())
+    }
+
+    @Test
+    fun validCAN() {
+        navigateToCANView()
+        onView(withText(R.string.can_helper_text)).check(matches(isDisplayed()))
+        onView(supportsInputMethods()).perform(typeText("123456"))
+        onView(withText(R.string.can_delete)).perform(closeSoftKeyboard())
+
+        onView(withText(R.string.can_status_saved)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun invalidCAN() {
+        navigateToCANView()
+        onView(supportsInputMethods()).perform(typeText("12345"))
+        onView(withText(R.string.can_helper_text)).check(matches(isDisplayed()))
+    }
+}
